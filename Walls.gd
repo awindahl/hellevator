7585d1e8ae736efdd8b6f0f5ea1503f882b04f17
awindahl <awindahl@kth.se>
2018-09-13 17:25:41 +0200

    added deathwall and fixed platform spawns

--- Walls.gd
+++ Walls.gd
@@ -1,6 +1,7 @@
 extends StaticBody2D
 
 var hasSpawned = false
+onready var player = get_parent().get_node("Player")
 
 func _process(delta):
 	var wallL = get_node("Wall5")
@@ -8,7 +9,6 @@ func _process(delta):
 	var Rstartpos = wallR.global_position
 	var Lstartpos = wallL.global_position
 	var playerPos = get_parent().get_node("Player").global_position
-	var player = get_parent().get_node("Player")
 	var spawner = get_node("Spawner")
 	var spawnPos = spawner.global_position
 	var platform = load("res://Platform.tscn")
@@ -20,13 +20,15 @@ func _process(delta):
 	spawner.global_position = Vector2(spawnPos.x,spawnPos.y)
 
 	randomize()
-	var randInt = rand_range(-150,700)
+	var randInt = rand_range(-50,50)
 	
 	if player.linear_velocity.y > 400 and !hasSpawned:
 		hasSpawned = true
 		platformInst.set_name(var2str(player.distance))
-		platformInst.global_position = Vector2(randInt,playerPos.y+1150)
-		spawner.get_parent().get_parent().add_child(platformInst)
+		platformInst.global_position = Vector2(randInt,playerPos.y + 1150)
+		spawner.get_parent().add_child(platformInst)
 	
 func _on_Timer_timeout():
-	hasSpawned = false
+	if player.linear_velocity.y > 400:
+		print("timed out, spawning")
+		hasSpawned = false
